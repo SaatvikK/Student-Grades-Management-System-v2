c16ab04d101ace14498a5d1ab8e3e86773249085
saatvikk <saatvikrk@gmail.com>
2023-07-06 01:43:02 +0100

    main functionality added

---
+++ src/prototype/cmds.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	//"os"
+	//"reflect"
+	"strconv"
+	//"strings"
+	//"io"
+	//"log"
+	//"net/http"
+)
+
+func createStudent(forename string, surname string, id int) Student {
+	newStudent := Student{Id: id, Forename: forename, Surname: surname}
+	return newStudent
+}
+
+func addGradeCmd(reader *bufio.Reader) {
+	fmt.Println("== Adding a Grade to a Student ==")
+	fmt.Print("| Student ID: ")
+	id, _ := strconv.ParseInt(stdInput(reader), 10, 64)
+	for i := 0; i < len(students); i++ {
+		if students[i].Id == int(id) {
+			student := students[i]
+			fmt.Print("| Subject Name: ")
+			subjectName := stdInput(reader)
+			fmt.Print("| Grade: ")
+			grade, _ := strconv.ParseFloat(stdInput(reader), 64)
+
+			newSubject := &Subject{
+				Name:  subjectName,
+				Grade: grade,
+			}
+
+			student.Subjects = append(student.Subjects, *newSubject)
+			students[i] = student
+			break
+		}
+	}
+
+	fmt.Println("| Added subject & grade to student.")
+}
+
+func addStudentCmd(reader *bufio.Reader) {
+	fmt.Println("== Adding a New Student ==")
+	fmt.Print("| Forename: ")
+	forename := stdInput(reader)
+	fmt.Print("| Surname: ")
+	surname := stdInput(reader)
+
+	createdStudent := createStudent(forename, surname, idGenerator())
+	students = append(students, createdStudent)
+}
+
+func seeStudentCmd(reader *bufio.Reader) {
+	fmt.Println("== See a Student's Transcript ==")
+	fmt.Print("| Student ID: ")
+	id, _ := strconv.ParseInt(stdInput(reader), 10, 64)
+	for i := 0; i < len(students); i++ {
+		if students[i].Id == int(id) {
+			student := students[i]
+			fmt.Println("--- " + student.Forename + " " + student.Surname + " ---")
+			fmt.Println("| Student ID: " + strconv.Itoa(student.Id))
+			fmt.Println("| Student Name: " + student.Forename + " " + student.Surname)
+			fmt.Println("| Subject(s) Enrolled: ")
+			for j := 0; j < len(student.Subjects); j++ {
+				subject := student.Subjects[j]
+				fmt.Println("|| " + subject.Name + ": " + strconv.FormatFloat(subject.Grade, 'f', 0, 64))
+			}
+			fmt.Println("-----------------")
+		}
+	}
+}
---
+++ src/prototype/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	//"strconv"
+	"strings"
+	//"io"
+	//"log"
+	//"net/http"
+)
+
+type Subject struct {
+	Name  string
+	Grade float64
+}
+
+type Student struct {
+	Id       int
+	Forename string
+	Surname  string
+	Subjects []Subject
+}
+
+var students = []Student{} // temp cache database
+
+// After the database is created, this function will increment the highest ID in the database.
+// However, for now it will just assign 1 as the ID.
+func idGenerator() int {
+	return 1
+}
+
+func stdInput(reader *bufio.Reader) string {
+	cmd, _ := reader.ReadString('\n')
+	return strings.Replace(cmd, "\n", "", -1)
+}
+
+func cmdHandler(cmd string, reader *bufio.Reader) {
+	if strings.Compare("help", cmd) == 0 {
+		fmt.Println("hai")
+
+	} else if strings.Compare("addstudent", cmd) == 0 {
+		addStudentCmd(reader)
+
+	} else if strings.Compare("addgrade", cmd) == 0 {
+		addGradeCmd(reader)
+
+	} else if strings.Compare("seestudent", cmd) == 0 {
+		seeStudentCmd(reader)
+	}
+}
+
+func main() {
+	fmt.Println("Welcome to the SGMS v1!")
+	fmt.Println("Please type 'help' to get a list of commands!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(reflect.TypeOf(reader))
+
+	for {
+		fmt.Print("$ ")
+		cmd, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		cmd = strings.Replace(cmd, "\n", "", -1)
+		cmdHandler(cmd, reader)
+	}
+
+}
